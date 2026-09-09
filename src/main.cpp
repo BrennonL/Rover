ebc0ce67298650088b3fcae4459d669d9f768aec
BrennonL <brennon.laney@gmail.com>
2026-09-09 11:15:00 -0600

    feat:blinking light

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
-#include <avr.io.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+void led_on() {
+  PORTB |= (1<<PB5);
+}
+void led_off() {
+  PORTB &= ~(1<<PB5);
+}
+
+void delay_ms(int ms) {
+  while (ms --) {
+    _delay_ms(1);
+  }
+}
 
 int main() {
   while (true) {
-    
+    led_on();
+    delay_ms(3000000);
+    led_off();
+    delay_ms(3000000);
   }
   return 0;
 }
